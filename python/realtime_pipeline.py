a47f3f424a77311184b66d885ca6010f87aec2fe
PhaseResponse <148216930+PhaseResponse@users.noreply.github.com>
2026-08-05 15:00:30 +0300

    parameter comments

--- python/realtime_pipeline.py
+++ python/realtime_pipeline.py
@@ -23,7 +23,12 @@ samples_to_spectrogram()  --> spectrogram, clipped and normalized [0,1]
 import numpy as np
 from scipy.signal import spectrogram as sg
 
-#--- parameters ------------#
+#--- user configurable parameters ------------#
+N_FFT_HOPS = 4            # hop size in units of N_FFT. Default 256ms.
+DETECTION_THRESHOLD = 0.974  # drone/no-drone detection threshold, 6 on level_to_threshold scale
+
+
+#--- model parameters ------------#
 TARGET_RMS = 0.1          # mic fusion target RMS level (relative to full scale)
 MAX_GAIN = 3.0            # mic fusion gain multiplier
 COMP_THRESHOLD = 0.9      # compression threshold 
@@ -37,9 +42,6 @@ VMIN = -150               # spectrogram min power [dB]
 VMAX = -80                # spectrogram max power [dB]
 
 N_SEGMENT = N_FFT * 20    # samples per segment. 1.28s @ 16kHz.
-N_FFT_HOPS = 4            # hop size in units of N_FFT. Default 256ms.
-
-DETECTION_THRESHOLD = 0.974  # drone/no-drone detection threshold, 6 on level_to_threshold scale
 
 
 def combine_channels(buffer, channels=None):
